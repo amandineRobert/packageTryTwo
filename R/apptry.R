f1cc4f690d0e4794ff3c9b8bdc551880493ba303
Amandine Robert <amandiner@dh012142.liv.ac.uk>
2019-05-28 13:56:29 +0100

    adding my simple shiny app

---
+++ R/apptry.R
@@ -0,0 +1,33 @@
+library(shiny)
+source("./packageTry.R")
+ui <- fluidPage(
+  h1("1st Shiny app"),
+  flowLayout(
+    numericInput(inputId = "num1",
+                 "Number 1", value = 0),
+    # uiOutput("n1"),
+
+    numericInput(inputId = "num2",
+                 "Number 2", value = 0),
+    # uiOutput("n2"),
+
+    actionButton(inputId = "add",
+                 label = "Add"),
+
+
+    textOutput("res")
+  )
+)
+
+server <- function(input, output){
+
+  result <- eventReactive(input$add, {
+    addme(input$num1, input$num2)
+  })
+  output$res <- renderText({
+    result()
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
